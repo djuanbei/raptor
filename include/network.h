695de089c2c3abfdb4907ad26e969fa492743434
liyun dai <dlyun2002@gmail.com>
2017-11-21 09:52:00 +0800

    ok

---
+++ include/network.h
@@ -0,0 +1,11 @@
+
+
+/**
+ * @file   network.h
+ * @author Liyun Dai <dlyun2009@gmail.com>
+ * @date   Thu Nov  9 10:39:55 2017
+ *
+ * @brief  network topology and other static network property
+ *
+ *
+ */
---
+++ include/sparse.h
@@ -0,0 +1,28 @@
+/**
+ * @file   sparse.h
+ * @author Liyun Dai <dlyun2009@gmail.com>
+ * @date   Mon Nov 20 19:36:04 2017
+ * 
+ * @brief  sparse linear equation  solver Ax=b where A is a sparse matrix and b is sparse vector
+ * 
+ * 
+ */
+
+#include"config.h"
+#include<vector>
+
+namespace raptor {
+namespace sparse{
+using namespace std;
+
+class SparseSolver{
+ private:
+  vector<>
+  
+};
+
+
+}
+
+
+}
